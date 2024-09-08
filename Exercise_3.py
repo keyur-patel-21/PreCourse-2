df76a8c2f4ea35c1d0f7a3feee1710eb925c3fa2
keyur_patel <keyur.patel@simformsolutions.com>
2024-09-08 17:44:07 -0400

    implemented exercise 3

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,21 +1,47 @@
+# Time complexity : O(n)
+# Space Complexity : O(1)
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current =  self.head
+            
+            while current.next:
+                current = current.next
+            current.next = new_node
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        if self.head is None:
+            print("The list is empty")
+            return
 
+        slow = self.head
+        fast = self.head
+
+        # Fast pointer moves two steps, slow pointer moves one step
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        print("The middle element is:", slow.data)
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
